64dbda1493496c2beac1fa8430959a9e73bb7004
mel <caramel.xm99@gmail.com>
2022-05-27 14:23:24 +0700

    UPDATE responsive

--- src/views/Login_Teacher.vue
+++ src/views/Login_Teacher.vue
@@ -1,53 +1,49 @@
 <template>
-  <div class="bg-light">
-    <div class="grid grid-cols-2 h-full">
+  <div class="background sm:bg-light bg-white">
+    <div class="md:grid md:grid-cols-2 h-full">
       <div>
         <img
           src="../assets/LoginHelio.png"
-          class="flex justify-center place-content-center h-screen"
+          class="justify-center place-content-center h-screen md:block hidden"
         />
       </div>
 
+      <div class="box bg-white">
+        <div class="pt-20 text-xl font-bold text-blue-700 justify-center flex">
+          <h1>เข้าสู่ระบบ</h1>
+        </div>
 
-        <div class="box bg-white">
-          <div
-            class="pt-20 text-xl font-bold text-blue-700 justify-center flex"
-          >
-            <h1>เข้าสู่ระบบ</h1>
+        <form @submit.prevent="login" class="">
+          <div class="pt-2">
+            <div class="flex justify-center mt-20">
+              <span class="material-symbols-outlined"> alternate_email </span>
+              <input
+                v-model="user"
+                type="text"
+                placeholder="อีเมล"
+                class="border-b border-babyblue"
+              />
+            </div>
+            <div class="flex justify-center mt-10">
+              <span class="material-symbols-outlined"> lock </span>
+              <input
+                v-model="pass"
+                type="password"
+                placeholder="รหัสผ่าน"
+                class="border-b border-babyblue"
+              />
+            </div>
           </div>
 
-          <form @submit.prevent="login" class="">
-            <div class="pt-2">
-              <div class="flex justify-center mt-20">
-                <span class="material-symbols-outlined"> alternate_email </span>
-                <input
-                  v-model="user"
-                  type="text"
-                  placeholder="อีเมล"
-                  class="border-b border-babyblue"
-                />
-              </div>
-              <div class="flex justify-center mt-10">
-                <span class="material-symbols-outlined"> lock </span>
-                <input
-                  v-model="pass"
-                  type="password"
-                  placeholder="รหัสผ่าน"
-                  class="border-b border-babyblue"
-                />
-              </div>
-            </div>
-
-            <div class="flex justify-center mt-20" @click="login">
-              <button class="bg-primary rounded-md w-40 justify-center flex">
-                <p class="text-white">เข้าสู่ระบบ</p>
-              </button>
-            </div>
-          </form>
-        </div>
+          <div class="flex justify-center mt-20" @click="login">
+            <button class="bg-primary rounded-md w-40 justify-center flex">
+              <p class="text-white">เข้าสู่ระบบ</p>
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-
+  </div>
 </template>
 
 <script>
@@ -67,10 +63,10 @@ export default {
     async login() {
       if (!this.user || !this.pass) {
         alert("กรุณากรอก อีเมล และ รหัสผ่าน");
-      } 
+      }
       // else if (this.user != this.url || this.pass != this.url) {
       //   alert("อีเมล หรือ รหัสผ่าน ไม่ถูกต้อง");
-      // } 
+      // }
       else {
         axios
           .post(this.url, {
@@ -80,7 +76,6 @@ export default {
           .then((res) => {
             console.log(res.data);
             if (res.data.statusCode === 200) {
-              
               localStorage.setItem("token", res.data.data.token);
               console.log(res.data.data.token);
               return this.$router.push("/helioscore");
@@ -99,17 +94,20 @@ export default {
 ::placeholder {
   color: #b3dbfb;
 }
-img{
-  @apply xl:px-20 xl:py-20 
+.background {
+  @apply sm:pt-20 md:pt-0;
+}
+img {
+  @apply h-auto
+  xl:px-20 xl:py-20 
   lg:px-20 lg:py-36
   md:px-12 md:py-28;
 }
 .box {
-  box-shadow: 0px 0px 50px 0px #d2ebff;
-  border-radius: 20px;
   @apply xl:mr-80 xl:my-20
   h-3/4 lg:my-20 lg:mr-28
-  md:my-16 md:mr-20;
+  md:my-16 md:mr-20 md:mx-0 md:rounded-xl
+  sm:justify-center sm:mx-40;
 }
 span {
   color: #b3dbfb;
